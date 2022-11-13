34d06efd26fe14c550af0bb23e2e59309835dba0
Alex5418 <102533981+Alex5418@users.noreply.github.com>
2022-11-13 15:16:55 -0600

    Update and rename rout.cpp to route.cpp

--- route.cpp
+++ route.cpp
@@ -1,6 +1,6 @@
-#include "rout.h"
+#include "route.h"
 
-rout::rout() {
+route::route() {
     airline_ = "";
     airline_id_ = 0;
     source_airport_ "";
@@ -11,7 +11,7 @@ rout::rout() {
     equipment = "";
 }
 
-rout::rout(string airline,
+rout::route(string airline,
     int airline_id,
     string source_airport,
     int source_airport_id,
@@ -33,55 +33,55 @@ void rout::set_airline(string airline) {
     airline_ = airline;
 }
 
-void rout::set_airline_id(int airline_id) {
+void route::set_airline_id(int airline_id) {
     airline_id_ = airline_id;
 }
 
-void rout::set_source_airport(string source_airport) {
+void route::set_source_airport(string source_airport) {
     source_airport_ = source_airport;
 }
 
-void rout::set_source_airport_id(int source_airport_id) {
+void route::set_source_airport_id(int source_airport_id) {
     source_airport_id_ = source_airport_id;
 }
 
-void rout::set_destination_airport(string destination_airport) {
+void route::set_destination_airport(string destination_airport) {
     destination_airport_ = destination_airport;
 }
 
-void rout::set_destination_airport_id(int destination_airport_id) {
+void route::set_destination_airport_id(int destination_airport_id) {
     destination_airport_id_ = destination_airport_id;
 }
 
-void rout::set_codeshare(bool codeshare) {
+void route::set_codeshare(bool codeshare) {
     codeshare_ = codeshare;
 }
 
-void rout::set_equipment(string equipment_) {
+void route::set_equipment(string equipment_) {
     equipment_ = equipment;
 }
 
-string rout::get_airline() [
+string route::get_airline() [
     return airline_;
 ]
-int rout::get_airline_id() {
+int route::get_airline_id() {
     return airline_id_;
 }
-string rout::get_source_airport() {
+string route::get_source_airport() {
     return source_airport_;
 }
-int rout::get_source_airport_id() {
+int route::get_source_airport_id() {
     return source_airport_id_;
 }
-string rout::get_destination_airport() {
+string route::get_destination_airport() {
     return destination_airport_;
 }
-int rout::get_destination_airport_id() {
+int route::get_destination_airport_id() {
     return destination_airport_id_;
 }
-bool rout::get_codeshare() {
+bool route::get_codeshare() {
     return codeshare_;
 }
-string rout::get_equipment() {
+string route::get_equipment() {
     return equipment_;
 }
