7ed044ce8e62b6d3e0c72bc290a99cffbb80efc8
Alex5418 <102533981+Alex5418@users.noreply.github.com>
2022-11-22 17:31:32 -0600

    delete test and implement BFS

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -10,4 +10,27 @@
 #include "Utils.h"
 
 // Breadth fisrt search algorithm
+void Graph::BFS(int startVertex) {
+    std::queue<int> q;
+    q.push(startVertex);
+    visited[startVertex] = true;
+    parent[startVertex] = -1;
+
+    while (!q.empty()) {
+        int currentVertex = q.front();
+        q.pop();
+        std::cout << "Visited " << currentVertex << std::endl;
+
+        for (int i = 0; i < adjLists[currentVertex].size(); i++) {
+            int adjVertex = adjLists[currentVertex][i];
+
+            if (!visited[adjVertex]) {
+                q.push(adjVertex);
+                visited[adjVertex] = true;
+                parent[adjVertex] = currentVertex;
+            }
+        }
+    }
+}
+
 
