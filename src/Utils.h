a45c1fd77f4f640accf09bb50eedf5b13d5bbfab
Alex5418 <102533981+Alex5418@users.noreply.github.com>
2022-11-22 17:29:06 -0600

    Update Utils.h

--- src/Utils.h
+++ src/Utils.h
@@ -13,11 +13,28 @@ class Graph
 private:
     /* data */
     int numVertices;
+
+    // adjacency list
+    std::vector<std::vector<int>> adjLists;
+
+    // visited array
+    std::vector<bool> visited;
+
+    // parent array
+    std::vector<int> parent;
+
+    // distance array (weight)
+    std::vector<int> distance;
+
+
+
 public:
     Graph(int numVertices);
     void addEdge(int source, int destination);
-    void BFS(int startVertex);
-    // void printShortestDistance(int sourceVertex, int destinationVertex);
+    void BFS(int startVertex);  // Breadth First Search to traverse the graph
+    void Djikstra(int sourceVertex, int destinationVertex); // Djikstra's algorithm to find the shortest path
+    void printShortestDistance(int sourceVertex, int destinationVertex);
+
 
     ~Graph();
 };
