c55a1f7dff549460996ae20cdf6bbd96fab6f336
Sirui-Zh <87266014+Sirui-Zh@users.noreply.github.com>
2022-11-22 18:55:27 -0600

    Update utilities.h

--- src/utilities.h
+++ src/utilities.h
@@ -7,8 +7,7 @@ class graph {
 public:
     int isNotVisited(int x, vector<int>& path);
     void BFS(vector<vector<int> >& g, int dep, int dst);
-    int bfs();
+    
 private:
-    const vector<Airport>* airports_ptr_;
-    const vector<Route>* routes_ptr_;
+    
 };
