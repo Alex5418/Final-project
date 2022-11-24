78a8783ad59e5e32b4ddb1c07fd6ffe362defc90
Sirui-Zh <87266014+Sirui-Zh@users.noreply.github.com>
2022-11-23 21:16:39 -0600

    Update main.cpp

--- entry/main.cpp
+++ entry/main.cpp
@@ -7,59 +7,48 @@
 #include "route.h"
 #include "filereading.h"
 #include "utilities.h"
-#include "Graph.h"
 
 using namespace std;
 
 int main () {
     
-
+    /*readfile*/
     filereading fr;
-
-    fr.readairport("/workspaces/CS225/Final-project/data/airports.txt");
-    fr.readairroute("/workspaces/CS225/Final-project/data/Routes.txt");
-    // fr.print_route();
+    fr.readairport("/workspaces/cs225/Final-project/data/airports.txt");
+    fr.readairroute("/workspaces/cs225/Final-project/data/tem.txt");
+    
+    /*BFS*/
     vector<vector<int> > g;
-    //test distance calculation method
+    graph gra;
+    g.resize(14111);
+    for (int i = 0; i < fr.get_route_size(); i++) {
+        int tem_destination_id = fr.getRouteVector()[i].get_destination_airport_id();
+        int tem_source_id = fr.getRouteVector()[i].get_source_airport_id();
+        if (fr.getRouteVector()[i].get_source_airport_id() == 0) {
+            tem_source_id = fr.IATA_to_airpot_id(fr.getRouteVector().at(i).get_source_airport());
+        }
+        if (fr.getRouteVector()[i].get_destination_airport_id() == 0) {
+            tem_destination_id = fr.IATA_to_airpot_id(fr.getRouteVector()[i].get_destination_airport());
+        }
+        g[tem_source_id].push_back(tem_destination_id);
+    }
+    gra.BFS(g, 2990, 4078);
 
-    // g.resize(4);
-    // graph gra;
-    // construct a graph
-    // g = {
-    //     {3,1,2},
-    //     {3},
-    //     {1,0},
-    //     {}
-    // };
-    // gra.BFS(g, 5508, 5499);
-
-
-    //get latitude and longtitude of two airports
-    // double lat1 = fr.getAirportVector()[0].get_latitude();
-    // double long1 = fr.getAirportVector()[0].get_longtitude();
-    // double lat2 = fr.getAirportVector()[1].get_latitude();
-    // double long2 = fr.getAirportVector()[1].get_longtitude();
-
-    // double tmp = fr.distance(lat1, long1, lat2, long2);
-
-    // fr.print_airport();
-
-    // std::cout << tmp << std::endl;
-
-
-
-
-    Graph G(fr.getAirportVector(), fr.getRouteVector());
-    
-
-    G.printGraph("full_test.txt");
-
-    //G.printConnectedAirports(4608);
-
-    //test BFS to find route
-    // G.BFS(2965, 2990);
-
-    
-
-    return 0;
+    /*Dijkstra*/
+    vector<vector<double>> g1;
+    g1.resize(14111);
+    for (int tem_rows = 0; tem_rows < 14111; tem_rows++)
+        g1[tem_rows].resize(14111);
+    for (int i = 0; i < fr.get_route_size(); i++) { 
+        int tem_destination_id = fr.getRouteVector()[i].get_destination_airport_id();
+        int tem_source_id = fr.getRouteVector()[i].get_source_airport_id();
+        if (fr.getRouteVector()[i].get_source_airport_id() == 0) {
+            tem_source_id = fr.IATA_to_airpot_id(fr.getRouteVector().at(i).get_source_airport());
+        }
+        if (fr.getRouteVector()[i].get_destination_airport_id() == 0) {
+            tem_destination_id = fr.IATA_to_airpot_id(fr.getRouteVector()[i].get_destination_airport());
+        }
+        g1[tem_source_id][tem_destination_id] = fr.distance(tem_source_id, tem_destination_id);
+    }
+    gra.dijkstra(g1, 2990, 4078);
 }
