cd91d72aa6ec91662a86330995bf41c93bc72de5
Alex5418 <102533981+Alex5418@users.noreply.github.com>
2022-12-03 16:07:47 -0600

    test

--- build/test_tests-b12d07c.cmake
+++ build/test_tests-b12d07c.cmake
@@ -1,5 +1,5 @@
 add_test( [==[test for test case]==] /workspaces/CS225/Final-project/build/test [==[test for test case]==]  )
 set_tests_properties( [==[test for test case]==] PROPERTIES WORKING_DIRECTORY /workspaces/CS225/Final-project/build)
-add_test( [==[test graph of airports]==] /workspaces/CS225/Final-project/build/test [==[test graph of airports]==]  )
-set_tests_properties( [==[test graph of airports]==] PROPERTIES WORKING_DIRECTORY /workspaces/CS225/Final-project/build)
-set( test_TESTS [==[test for test case]==] [==[test graph of airports]==])
+add_test( [==[test read data from file]==] /workspaces/CS225/Final-project/build/test [==[test read data from file]==]  )
+set_tests_properties( [==[test read data from file]==] PROPERTIES WORKING_DIRECTORY /workspaces/CS225/Final-project/build)
+set( test_TESTS [==[test for test case]==] [==[test read data from file]==])
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
 
 #include "/workspaces/CS225/Final-project/src/Airport.h"
 #include "/workspaces/CS225/Final-project/src/route.h"
@@ -15,16 +16,26 @@
 
 // using namespace cs225;
 
+
+
+
 TEST_CASE("test for test case", "[weight=0][part=1]") {
 
   REQUIRE( true == true );
+
 }
 
-TEST_CASE("test graph of airports", "[weight=0][part=1]") {
+TEST_CASE("test read data from file", "[weight=0][part=1]") {
+
+  filereading test_fr;
+  test_fr.readairport("/workspaces/CS225/Final-project/data/Airports_test");
+  std::vector<airport> test_airport_vec = test_fr.getAirportVector();
+  for (auto a : test_airport_vec) {
+    std::cout << a.get_name() << std::endl;
+  }
+  REQUIRE( test_airport_vec.size() == 27 );
 
 
-
-    REQUIRE (true == true);
 
 
 }
